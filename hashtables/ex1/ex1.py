cba989d3c2392632ea96246d0a70989cad7741a7
farjadfazli <farjadfazli@gmail.com>
2020-09-04 12:05:41 -0400

    completed ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+    if (length == 2) and (weights[0] + weights[1] == limit):
+        return [1, 0]
+    elif (length == 2):
+        return None
+
+    hash_table = {weight:index for index, weight in enumerate(weights)}
+
+    output = []
+
+    for weight in weights:
+        if (limit - weight) in hash_table:
+            output.append(hash_table[limit-weight])
+    
+    if len(output) < 2:
+        return None
+    else:
+        return output
